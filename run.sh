90452525fa6899db2d1049b158b022fd98fa0d3e
kwwossjowowee <istegebagli830@gmail.com>
2024-08-16 14:03:46 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-pip install selenium notebook fake-useragent
+pip install selenium notebook fake-useragent --break-system-packages
 echo -e "e\ne" | jupyter notebook password 
 jupyter notebook --ip=0.0.0.0 --port=8080 --allow-root
